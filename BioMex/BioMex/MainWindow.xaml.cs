537ac27a0d284bc21609422cc2dfe441fb7cf7ea
Jean-Jacques Byleveld <jean.j@live.com>
2020-01-15 20:51:54 +0200

    Add new solution with Emgu not working

---
+++ BioMex/BioMex/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Emgu.CV;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+
+namespace BioMex
+{
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartFacialRecognition()
+        {
+            var cascadeClassifier = new CascadeClassifier("J:/Develop/BioMex/BioMex/BioMex/HaarCascade/haarcascade_frontalface_default.xml");
+            var capture = new VideoCapture();
+            var faceRecognizer = new EigenFaceRecognizer();
+        }
+    }
+}
